56ff6226f44e2cc53acfddbe53028a60db8751e8
MagicSoft <magicsoft.uz@gmail.com>
2022-06-23 11:09:36 +0500

    Sign in and Sing out Buttons complete

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,41 +1,64 @@
 import React, { useEffect, useState } from "react";
 
-const GoogleAuth = () => {
-  const [state, setState] = useState("");
-  const [kirganmi, setKirganmi] = useState(null);
-
-  useEffect(() => {
-    window.gapi.load("auth2", function () {
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+  componentDidMount() {
+    window.gapi.load("auth2", () => {
       window.gapi.auth2
         .init({
           client_id:
             "981717567391-bso3mhv9u0bju97m61p7hddfhht8emgs.apps.googleusercontent.com",
-          scope: "email",
-          plugin_name: "TwitchClone",
+
+          plugin_name: "App Name that you used in google developer console API",
         })
         .then(() => {
-          setState(window.gapi.auth2.getAuthInstance());
-          // console.log(state);
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        })
+        .catch((err) => {
+          console.log(err);
         });
     });
-  }, []);
+  }
 
-  const onAuthChange = () => {
-    setKirganmi({ kirganYokiKirmagan: state.isSignedIn.get() });
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
-  const kirishFunc = () => {
-    if (kirganmi === null) {
-      <div>Kutishda</div>;
-    }
-    if (kirganmi === true) {
-      <div>Kirgansiz</div>;
-    }
-    if (kirganmi === false) {
-      <div>Kirmagansiz</div>;
-    }
+  onSignIn = () => {
+    this.auth.signIn();
   };
 
-  return <div>{kirishFunc()}</div>;
-};
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
+  renderButton() {
+    console.log(this.state);
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn === true) {
+      return (
+        <div>
+          <button onClick={this.onSignOut} className="ui red google button">
+            <i className="ui google icon" />
+            Sign out
+          </button>
+          <h3>{this.auth.currentUser.get().getBasicProfile().getName()}</h3>
+        </div>
+      );
+    } else if (this.state.isSignedIn === false) {
+      return (
+        <button onClick={this.onSignIn} className="ui green google button">
+          <i className="ui google icon" />
+          Sign in
+        </button>
+      );
+    }
+  }
+  render() {
+    return <div>{this.renderButton()} </div>;
+  }
+}
 export default GoogleAuth;
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { DatePicker, message, Alert, Progress } from "antd";
+import "antd/dist/antd.css";
 
 const StreamList = () => {
-  return <div>StreamList</div>;
+  return (
+    <div style={{ width: "40%", margin: "0 auto" }}>
+      <Progress percent={30} />
+      <Progress percent={50} status="active" />
+      <Progress percent={70} status="exception" />
+      <Progress percent={100} />
+      <Progress percent={50} showInfo={false} />
+    </div>
+  );
 };
 
 export default StreamList;
